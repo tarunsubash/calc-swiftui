7dfe503f470d2f56544e5ebd7bace55cbadd1730
Subash <tarun.subash01@gmail.com>
2020-05-02 11:35:16 +0530

    layout button grid and add background color

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -9,13 +9,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    let calculatorButtons = [["7", "8", "9", "X"],
+                             ["4", "5", "6", "-"],
+                             ["1", "2", "3", "+"],
+                             ["0", ".", ".", "="]
+    ]
+    
     var body: some View {
-        VStack {
-            HStack {
-                ForEach(["7", "8", "9", "X"], id: \.self) { button in
-                    Text(button)
-                        .font(.system(size: 32))
-                        .frame(width: 80, height: 80)
+        ZStack(alignment: .bottom) {
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack {
+                ForEach(calculatorButtons, id: \.self) {  row in
+                    HStack {
+                        ForEach(row, id: \.self) { button in
+                            Text(button)
+                                .font(.system(size: 32))
+                                .frame(width: 80, height: 80)
+                                .foregroundColor(.white)
+                        }
+                    }
                 }
             }
         }
